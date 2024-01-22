20101459cffd922f69c48967ced1f3be3841f85f
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-01-22 15:43:20 +0000

    created svg ladder with working notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import BassClef from './assets/Bass-Clef.png';
 import TrebleClef from './assets/treble-clef.png';
-import MusicPlayer from './components/music-player';
 import Header from './components/header';
 import Footer from './components/footer';
+import MusicPlayer from './components/music-player';
 
 function App() {
 
@@ -17,17 +17,8 @@ function App() {
             <p>This is a treble clef sign. It marks notes higher than middle C.</p>
             <img src={BassClef} alt="bass-clef" className="clef-img"></img>
             <p>This is a bass clef. It marks notes lower than middle C.</p>
-            <div className='ladder'>
-                        <svg width="100" height="300">
-                  <rect width="20" height="300" />
-                  <rect x="80" width="20" height="300" />
-                  <line x1="80" y1="50" x2="120" y2="50" stroke="black" />
-                  <line x1="80" y1="100" x2="120" y2="100" stroke="black"/>
-                  <line x1="80" y1="150" x2="120" y2="150" stroke="black" />
-            </svg>
-            </div>
+            <MusicPlayer></MusicPlayer>
         </div>
-        <MusicPlayer></MusicPlayer>
       </main>
     </div>
     <Footer></Footer>
--- frontend/src/components/music-player.tsx
+++ frontend/src/components/music-player.tsx
@@ -1,17 +1,31 @@
 import * as Tone from 'tone';
 
-function playNote() {
+function playNote(note: string) {
     // create a synth
     const synth = new Tone.Synth().toDestination();
     // play a note from that synth
-    synth.triggerAttackRelease("C4", "8n");
+    synth.triggerAttackRelease(note, "8n");
 }
 
 function MusicPlayer() {
     return (
-        <div className="music-player">
-             <button type="button" id="button" onClick={playNote}>Middle C</button>
-        </div>
+        <div className='ladder'>
+                        <svg width="20%" height="500">
+                  <rect y="10" width="20" height="262" className='rect' />
+                  <rect y="10" x="90%" width="20" height="262" className='rect'/>
+                  <circle cx="100" cy="60"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("D5")} />
+                  <circle cx="150" cy="110"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("B4")} />
+                  <circle cx="200" cy="160"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("G4")} />
+                  <circle cx="250" cy="210"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("E4")} />
+                  <circle cx="300" cy="260"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("C4")} />
+                  <line x1="0" y1="10" x2="90%" y2="10" stroke="black" strokeWidth="2" className='line' />
+                  <line x1="0" y1="60" x2="90%" y2="60" stroke="black" strokeWidth="2" className='line'/>
+                  <line x1="0" y1="110" x2="90%" y2="110" stroke="black" strokeWidth="2" className='line'/>
+                  <line x1="0" y1="160" x2="90%" y2="160" stroke="black" strokeWidth="2" className='line'/>
+                  <line x1="0" y1="210" x2="90%" y2="210" stroke="black" strokeWidth="2" className='line'/>
+                  <line x1="0" y1="260" x2="90%" y2="260" stroke="black" strokeWidth="2" className='line'/>
+            </svg>
+            </div>
     )
 }
 
