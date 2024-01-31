e3d5eb4791fbf895ed517316673756c0a6040849
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-01-31 18:18:34 +0000

    procedurally generate notes

--- frontend/src/components/music-player.tsx
+++ frontend/src/components/music-player.tsx
@@ -7,13 +7,23 @@ function playNote(note: string) {
     synth.triggerAttackRelease(note, "8n");
 }
 
+const notes = ["G5", "F5", "E5", "D5", "C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4"]
+
 function MusicPlayer() {
     return (
         <div className='ladder'>
-                        <svg width="20%" height="500">
-                  <rect y="9" width="20" height="262" className='rect' />
-                  <rect y="9" x="90%" width="20" height="262" className='rect'/>
-                  <circle cx="100" cy="60"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("D5")} />
+                        <svg width="20%" height="600">
+                  <rect y="50" width="20" height="262" className='rect' />
+                  <rect y="50" x="90%" width="20" height="262" className='rect'/>
+                  {notes.map((note, index)=> {
+                    return (
+                        <circle key={index} cx={50 + index*25} cy={35 + index*25} r="5" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote(note)}/>
+                    )
+                  })}
+                  {notes.map((note, index) => {
+                    return (<text key={index} x={30 + index*25} y={40 + index*25} className='svg-text'>{note[0]}</text>) 
+                  })}
+                  {/* <circle cx="100" cy="60"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("D5")} />
                   <circle cx="150" cy="110"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("B4")} />
                   <circle cx="200" cy="160"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("G4")} />
                   <circle cx="250" cy="210"  r="10" stroke="black" strokeWidth="3" fill="black" onClick={() => playNote("E4")} />
@@ -23,8 +33,7 @@ function MusicPlayer() {
                     <text x="70" y="105" className='svg-text'>B</text>
                     <text x="70" y="155" className='svg-text'>G</text>
                     <text x="70" y="205" className='svg-text'>E</text>
-                    <text x="70" y="255" className='svg-text'>C</text>
-                  <line x1="0" y1="10" x2="90%" y2="10" stroke="black" strokeWidth="2" className='line' />
+                    <text x="70" y="255" className='svg-text'>C</text> */}
                   <line x1="0" y1="60" x2="90%" y2="60" stroke="black" strokeWidth="2" className='line'/>
                   <line x1="0" y1="110" x2="90%" y2="110" stroke="black" strokeWidth="2" className='line'/>
                   <line x1="0" y1="160" x2="90%" y2="160" stroke="black" strokeWidth="2" className='line'/>
